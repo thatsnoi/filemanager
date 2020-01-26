9ac5659eb8021e9dc727735e0b358febc70df11a
Noah Jadallah <noah.jadallah@gmx.net>
2020-01-26 19:35:01 +0100

    Update api.js

--- packages/connector-google-drive-v2/src/api.js
+++ packages/connector-google-drive-v2/src/api.js
@@ -1,42 +1,5 @@
-import agent from 'superagent';
-import JSZip from 'jszip';
-import { serializePromises } from './utils/common';
-import { getDownloadParams } from './google-drive-utils';
-import parseRange from 'range-parser';
-
-async function appendGoogleApiScript() {
-  if (window.gapi) {
-    return false;
-  }
-
-  return new Promise((resolve, reject) => {
-    const script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'https://apis.google.com/js/api.js';
-    script.async = true;
-    script.defer = true;
-    document.body.appendChild(script);
-    script.addEventListener('load', () => {
-      console.log('Google API Script successfully fetched');
-      resolve();
-    });
-    script.addEventListener('error', (error) => {
-      console.log('Can\'t fetch Google API Script', error);
-      reject(error);
-    });
-  });
-}
-
-/**
- * Load the auth2 library.
- *
- * @returns {Promise<any>}
- */
-function loadAuth2Library() {
-  return new Promise(resolve => {
-    window.gapi.load('client:auth2', () => resolve());
-  });
-}
+import request from "superagent";
+import { normalizeResource } from "./utils/common";
 
 /**
  * hasSignedIn
@@ -44,303 +7,221 @@ function loadAuth2Library() {
  * @returns {boolean}
  */
 function hasSignedIn() {
-  return window.gapi.auth2.getAuthInstance().isSignedIn.get();
+  return true;
 }
 
 /**
- * Initializes the API client library and sets up sign-in state listeners.
+ * Init API
  *
- * @param options
- * @returns {Promise<{}>}
- */
-async function initClient(options) {
-  // This code doesn't work in IE11
-  await window.gapi.client.init({
-    apiKey: options.API_KEY,
-    clientId: options.CLIENT_ID,
-    discoveryDocs: options.DISCOVERY_DOCS,
-    scope: options.SCOPES
-  });
-  // End of not working code
-
-  if (!window.gapi.auth2.getAuthInstance()) {
-    console.log('Can\'t init Google API client');
-    return {
-      apiInitialized: false,
-      apiSignedIn: false
-    };
-  }
-
-  const isSignedIn = hasSignedIn();
-
-  if (isSignedIn) {
-    console.log('Google Drive sign-in Success');
-  } else {
-    console.log('Google Drive sign-in fail');
-  }
-
-  return {
-    apiInitialized: true,
-    apiSignedIn: isSignedIn
-  };
-}
-
-/**
- * Init Google API
- *
- * @param options
  * @returns {Promise<{apiInitialized: boolean, apiSignedIn: boolean}>}
  */
-async function init(options) {
-  await appendGoogleApiScript();
-  await loadAuth2Library();
-  console.log('Try auth on Google Drive API');
-  return await initClient(options); // Initializes API client library.
-}
-
-/**
- * Normalize Resource
- *
- * @param resource
- * @returns {{}} Normalized resource
- */
-function normalizeResource(resource) {
+function init() {
   return {
-    createdDate: Date.parse(resource.createdDate),
-    id: resource.id,
-    modifiedDate: Date.parse(resource.modifiedDate),
-    title: resource.title,
-    type: resource.mimeType === 'application/vnd.google-apps.folder' ? 'dir' : 'file',
-    size: typeof resource.fileSize === 'undefined' ? resource.fileSize : parseInt(resource.fileSize, 10),
-    parents: resource.parents,
-    capabilities: resource.capabilities,
-    downloadUrl: resource.downloadUrl,
-    mimeType: resource.mimeType,
-    exportLinks: resource.exportLinks
+    apiInitialized: true,
+    apiSignedIn: true
   };
 }
 
-async function getResourceById(options, id) {
-  const response = await window.gapi.client.drive.files.get({
-    fileId: id
-    // fields: 'createdDate,id,modifiedDate,title,mimeType,fileSize,parents,capabilities,downloadUrl'
-  });
-  const resource = normalizeResource({ ...response.result });
-  return resource;
-}
+function setupRequestOptions(options) {
+  let newOptions = { ...options };
 
-async function getParentsForId(options, id, result = []) {
-  if (id === 'root') {
-    return result;
+  if (!newOptions.header) {
+    newOptions.header = {};
   }
 
-  const response = await window.gapi.client.drive.parents.list({
-    fileId: id
-    // fields: 'items(id)'
-  });
-  const parentId = typeof response.result.items[0] === 'undefined' ? 'root' : response.result.items[0].id;
-
-  if (parentId === 'root') {
-    return result;
+  if (!newOptions.parameters) {
+    newOptions.parameters = {};
   }
 
-  const parent = await getResourceById(options, parentId);
-  return await getParentsForId(options, parentId, [parent].concat(result));
-}
-
-async function getParentIdForResource(options, resource) {
-  if (!resource.parents.length) {
-    return 'root';
-  }
-
-  return resource.parents[0].id;
-}
-
-async function getChildrenForId(options, { id, sortBy = 'title', sortDirection = 'ASC' }) {
-  const response = await window.gapi.client.drive.files.list({
-    q: `'${id}' in parents and trashed = false`,
-    orderBy: `folder,${sortBy} ${sortDirection === 'ASC' ? '' : 'desc'}`
-    // fields: 'items(createdDate,id,modifiedDate,title,mimeType,fileSize,parents,capabilities,downloadUrl)'
-  });
-  return response.result.items.map((o) => normalizeResource({ ...o }));
+  return newOptions;
 }
 
 async function getCapabilitiesForResource(options, resource) {
   return resource.capabilities || [];
 }
 
-async function downloadResource({ resource, params, onProgress, i = 0, l = 1 }) {
-  const accessToken = window.gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse().access_token;
+async function getResourceById(options, id) {
+  const route = `${options.apiRoot}/files/${id}`;
+  const method = "GET";
+  const requestOptions = setupRequestOptions(options);
+  const response = await request(method, route)
+    .set(requestOptions.header)
+    .query(requestOptions.parameters);
+  return normalizeResource(response.body);
+}
 
-  const { downloadUrl, direct, mimeType, fileName } = params;
+async function getChildrenForId(
+  options,
+  { id, sortBy = "name", sortDirection = "ASC" }
+) {
+  const requestOptions = setupRequestOptions(options);
+  const route = `${options.apiRoot}/files/${id}/children?orderBy=${sortBy}&orderDirection=${sortDirection}`;
+  const method = "GET";
+  const response = await request(method, route)
+    .set(requestOptions.header)
+    .query(requestOptions.parameters);
+  return response.body.items.map(normalizeResource);
+}
 
-  if (direct) {
-    return {
-      downloadUrl,
-      direct,
-      mimeType
-    }
+async function getParentsForId(options, id, result = []) {
+  if (!id) {
+    return result;
   }
 
-  let res;
+  let resource = await getResourceById(options, id);
 
-  try {
-    res = await agent.get(downloadUrl).
-      set('Authorization', `Bearer ${accessToken}`).
-      responseType('blob').
-      on('progress', event => {
-        onProgress((i * 100 + event.percent) / l)
-      });
-  } catch (err) {
-    throw new Error(`Failed to download resource: ${err}`);
+  if (!resource) {
+    return result;
   }
 
-  return {
-    direct,
-    file: res.body,
-    fileName
-  };
-}
+  let parentId = resource.parentId;
 
-async function downloadResources({ resources, apiOptions, onProgress }) {
-  // multiple resources -> download one by one
-  const files = await serializePromises({
-    series: resources.map(
-      resource => ({ onProgress, i, l }) => downloadResource({
-        resource,
-        params: {
-          ...getDownloadParams(resource),
-          direct: false
-        },
-        onProgress, i, l
-      })
-    ),
-    onProgress
-  });
-
-  onProgress(100);
-
-  const zip = new JSZip();
-  // add generated files to a zip bundle
-  files.forEach(({ fileName, file }) => zip.file(fileName, file));
-
-  const blob = await zip.generateAsync({ type: 'blob' });
-
-  return {
-    direct: false,
-    file: blob,
-    fileName: apiOptions.archiveName || 'archive.zip'
+  if (!parentId) {
+    return result;
   }
+
+  let parent = await getResourceById(options, parentId);
+  return getParentsForId(options, resource.parentId, [parent, ...result]);
 }
 
-async function initResumableUploadSession({ name, size, parentId }) {
-  const accessToken = window.gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse().access_token;
-  const uploadUrl = `https://www.googleapis.com/upload/drive/v2/files?uploadType=resumable`;
-
-  const res = await agent.post(uploadUrl).
-    set('Authorization', `Bearer ${accessToken}`).
-    set('X-Upload-Content-Length', size).
-    send({ title: name, parents: [{ id: parentId }] });
-
-  return res.headers['location'];
+async function getBaseResource(options) {
+  const route = `${options.apiRoot}/files`;
+  const requestOptions = setupRequestOptions(options);
+  const response = await request
+    .get(route)
+    .set(requestOptions.header)
+    .query(requestOptions.parameters);
+  return normalizeResource(response.body);
 }
 
-async function uploadChunk({ sessionUrl, size, startByte, content }) {
-  return new Promise((resolve, reject) => {
-    const chunkSize = 256 * 1024 * 2;
-    const endByte = startByte + chunkSize < size ? startByte + chunkSize : size;
-
-    agent.put(sessionUrl).
-      set('Content-Range', `bytes ${startByte}-${endByte - 1}/${size}`).
-      set('Content-Encoding', 'base64').
-      send(btoa(content.slice(startByte, endByte))).
-      end((err, res) => {
-        // if (err) { } // pass
-        resolve(res);
-      });
-  });
-}
-
-async function getRootId() {
-  return 'root';
-}
-
-async function uploadFileToId(parentId, file, onProgress) {
-  const size = file.content.length;
-  const sessionUrl = await initResumableUploadSession({ name: file.name, size, parentId });
-  let startByte = 0;
-
-  while (startByte < size) {
-    const res = await uploadChunk({
-      sessionUrl,
-      size,
-      startByte,
-      content: file.content
-    });
-
-    if (res.status === 308) {
-      const range = parseRange(size, res.headers['range']);
-      startByte = range[0].end + 1;
-
-      const progress = startByte / (size / 100);
-      onProgress(progress);
-    }
-
-    if (res.status === 200 || res.status === 201) {
-      return res;
+async function getIdForPartPath(options, currId, pathArr) {
+  const resourceChildren = await getChildrenForId(options, { id: currId });
+  for (let i = 0; i < resourceChildren.length; i++) {
+    const resource = resourceChildren[i];
+    if (resource.name === pathArr[0]) {
+      if (pathArr.length === 1) {
+        return resource.id;
+      } else {
+        return getIdForPartPath(options, resource.id, pathArr.slice(1));
+      }
     }
   }
 
   return null;
 }
 
-async function createFolder(apiOptions, parentId, folderName) {
-  return await window.gapi.client.drive.files.insert({
-    title: folderName,
-    parents: [{ id: parentId }],
-    mimeType: 'application/vnd.google-apps.folder'
-  });
+async function getIdForPath(options, path) {
+  const resource = await getBaseResource(options);
+  const pathArr = path.split("/");
+
+  if (pathArr.length === 0 || pathArr.length === 1 || pathArr[0] !== "") {
+    return null;
+  }
+
+  if (pathArr.length === 2 && pathArr[1] === "") {
+    return resource.id;
+  }
+
+  return getIdForPartPath(options, resource.id, pathArr.slice(1));
 }
 
-async function renameResource(apiOptions, id, newName) {
-  return await window.gapi.client.drive.files.patch({
-    fileId: id,
-    title: newName
-  });
+async function getParentIdForResource(options, resource) {
+  return resource.parentId;
+}
+
+async function uploadFileToId({ apiOptions, parentId, file, onProgress }) {
+  let route = `${apiOptions.apiRoot}/files`;
+  const requestOptions = setupRequestOptions(apiOptions);
+
+  return request
+    .post(route)
+    .field("type", "file")
+    .field("parentId", parentId)
+    .attach("files", file.file, file.name)
+    .set(requestOptions.header)
+    .query(requestOptions.parameters)
+    .on("progress", event => {
+      onProgress(event.percent);
+    });
+}
+
+async function downloadResources({ apiOptions, resources, onProgress }) {
+  const downloadUrl = resources.reduce(
+    (url, resource, num) =>
+      url + (num === 0 ? "" : "&") + `items=${resource.id}`,
+    `${apiOptions.apiRoot}/download?`
+  );
+  const requestOptions = setupRequestOptions(apiOptions);
+  let res = await request
+    .get(downloadUrl)
+    .set(requestOptions.header)
+    .query(requestOptions.parameters)
+    .responseType("blob")
+    .on("progress", event => {
+      onProgress(event.percent);
+    });
+
+  return res.body;
+}
+
+async function createFolder(options, parentId, folderName) {
+  const route = `${options.apiRoot}/files`;
+  const method = "POST";
+  const params = {
+    parentId,
+    name: folderName,
+    type: "dir"
+  };
+  const requestOptions = setupRequestOptions(options);
+  return request(method, route)
+    .send(params)
+    .set(requestOptions.header)
+    .query(requestOptions.parameters);
 }
 
 function getResourceName(apiOptions, resource) {
-  return resource.title;
+  return resource.name;
 }
 
-async function removeResources() {
-
+async function renameResource(options, id, newName) {
+  const route = `${options.apiRoot}/files/${id}`;
+  const method = "PATCH";
+  const requestOptions = setupRequestOptions(options);
+  return request(method, route)
+    .type("application/json")
+    .send({ name: newName })
+    .set(requestOptions.header)
+    .query(requestOptions.parameters);
 }
 
-async function signIn() {
-  window.gapi.auth2.getAuthInstance().signIn();
+async function removeResource(options, resource) {
+  const route = `${options.apiRoot}/files/${resource.id}`;
+  const method = "DELETE";
+  const requestOptions = setupRequestOptions(options);
+  return request(method, route)
+    .set(requestOptions.header)
+    .query(requestOptions.parameters);
 }
 
-async function signOut() {
-  window.gapi.auth2.getAuthInstance().signOut();
+async function removeResources(options, selectedResources) {
+  return Promise.all(
+    selectedResources.map(resource => removeResource(options, resource))
+  );
 }
 
 export default {
   init,
   hasSignedIn,
+  getIdForPath,
   getResourceById,
+  getCapabilitiesForResource,
   getChildrenForId,
-  getRootId,
   getParentsForId,
   getParentIdForResource,
-  getCapabilitiesForResource,
   getResourceName,
   createFolder,
-  downloadResource,
   downloadResources,
-  uploadFileToId,
   renameResource,
   removeResources,
-  signIn,
-  signOut
+  uploadFileToId
 };
